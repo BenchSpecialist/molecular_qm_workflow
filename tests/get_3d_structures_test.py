2ba1c58854c5a93967d293e7b96f6570f9c32770
Renke Huang <renke.huang@ses.ai>
2025-05-05 05:25:39 -0400

    update test

--- tests/get_3d_structures_test.py
+++ tests/get_3d_structures_test.py
@@ -6,12 +6,10 @@ from mqc_pipeline import smiles_to_3d_structures_by_rdkit
 def test_smiles_to_3d_structures_by_rdkit():
     # Test with a valid SMILES string
     smiles = "C"
-    result = smiles_to_3d_structures_by_rdkit(smiles)
-    assert result.smiles == smiles
-    print(result.elements)
-    print(result.xyz)
-    assert result.elements == ['C', 'H', 'H', 'H', 'H']
-    assert result.xyz.shape == (5, 3)
+    st = smiles_to_3d_structures_by_rdkit(smiles)
+    assert st.smiles == smiles
+    assert st.elements == ['C', 'H', 'H', 'H', 'H']
+    assert st.xyz.shape == (5, 3)
 
     # Raise error when given an invalid SMILES string
     invalid_smiles = "INVALID_SMILES"
